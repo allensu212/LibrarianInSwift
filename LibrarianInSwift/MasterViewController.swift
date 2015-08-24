a8bf44b1548e9e6d427b9adb5b032b80a2809bae
Wei-Lun Su <wsu8@illinois.edu>
2015-08-24 17:23:16 -0400

    initial commit

---
+++ LibrarianInSwift/MasterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  LibrarianInSwift
+//
+//  Created by Wei-Lun Su on 2015/8/24.
+//  Copyright (c) 2015年 allensu. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+    
+}
+
