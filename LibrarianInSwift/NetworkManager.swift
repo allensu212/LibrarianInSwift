bdbd74c2d374777466c68c122a3ebdd6dec10f2f
Wei-Lun Su <wsu8@illinois.edu>
2015-08-24 19:42:18 -0400

    model created

---
+++ LibrarianInSwift/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  LibrarianInSwift
+//
+//  Created by Wei-Lun Su on 2015/8/24.
+//  Copyright (c) 2015年 allensu. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    //
+}
