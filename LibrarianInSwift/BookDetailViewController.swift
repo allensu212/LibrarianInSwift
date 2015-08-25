2e64644c12e501027f21f1614de5ae0a911fbe49
Wei-Lun Su <wsu8@illinois.edu>
2015-08-24 21:40:50 -0400

    UI helper methods in book detail controller

--- LibrarianInSwift/BookDetailViewController.swift
+++ LibrarianInSwift/BookDetailViewController.swift
@@ -20,13 +20,51 @@ class BookDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI(bookObject)
+        configureSubViews()
     }
+    
+    //MARK: UISetup
     
     func updateUI(book: Book){
         bookTitleLabel.text = book.bookTitle;
         authorLabel.text = book.author;
         bookTitleLabel.font = UIFont(name: FONT_MAIN, size: 16)
         authorLabel.font = UIFont(name: FONT_MAIN, size: 14)
-        //[self configureTextViewUIFromBook:book];
+        configureTextView(bookObject)
+    }
+    
+    func configureSubViews(){
+        containerView.layer.cornerRadius = 2.0
+        containerView.layer.shadowColor = UIColor.darkGrayColor().CGColor
+        containerView.layer.shadowOpacity = 0.7
+        containerView.layer.shadowOffset = CGSizeMake(0.1, 0.3)
+        containerView.layer.shadowRadius = 2.0
+        
+        checkOutButton.layer.cornerRadius = checkOutButton.frame.size.width / 2
+        checkOutButton.layer.shadowColor = UIColor.darkGrayColor().CGColor
+        checkOutButton.layer.shadowOpacity = 0.7
+        checkOutButton.layer.shadowOffset = CGSizeMake(0.5, 0.5)
+        checkOutButton.layer.shadowRadius = 4.0
+    }
+    
+    func configureTextView(book: Book){
+        let dateString = book.lastCheckedOut;
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy'-'MM'-'dd  HH':'mm':'ss"
+        let date = dateFormatter.dateFromString(dateString!)
+        dateFormatter.dateFormat = "MMM d, yyyy h:mm a"
+        let formattedString = dateFormatter.stringFromDate(date!)
+        
+        let publisherString = book.publisher;
+        let formattedPublisherString = bookObject.publisher ?? "Default"
+        
+        let categoriesString = book.categories;
+        let formattedCategoriesString = bookObject.categories ?? "Default"
+        
+        bookInfoTextView.text = "Publisher: \(formattedPublisherString)\nTags: \(formattedCategoriesString)\n\nLast Checked Out:\n\(book.lastCheckedOutBy!) @ \(formattedString)"
+        
+        bookInfoTextView.font = UIFont(name: FONT_MAIN, size: 12)
+        bookInfoTextView.textColor = UIColor.darkGrayColor()
+        bookInfoTextView.textAlignment = NSTextAlignment.Right
     }
 }
